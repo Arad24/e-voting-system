337de59b6cdc3f59a71382966b49a15b6bc0dd3c
Arad Danieli <arad242424@gmail.com>
2024-01-21 18:34:47 +0200

    Add server communicator

--- ElectChain/StringUtils.h
+++ ElectChain/StringUtils.h
@@ -2,6 +2,9 @@
 # include <iostream>
 # include <vector>
 # include <sstream>
+# include <algorithm>
+# include <cctype>
+# include <string>
 
 class StringUtils
 {
@@ -9,4 +12,7 @@ class StringUtils
         static std::string vecToString(std::vector<unsigned char> vector);
         static std::string charToString(char* charArr);
         static char* stringToChar(std::string str);
+        std::string toLower(const std::string& text);
+        std::vector<unsigned char> strToVec(const std::string& text);
+
 };
