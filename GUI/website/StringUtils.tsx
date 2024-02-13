a426529d9efd66c6af16629d1df212830805b6c2
Alex Matatov <alexmatatov4@gmail.com>
2024-02-13 18:45:05 +0000

    Upload New File

---
+++ GUI/website/StringUtils.tsx
@@ -0,0 +1,56 @@
+class StringUtils {
+    static nodesVectorToString(nodes: string[]): string {
+        let result = "[";
+        
+        for (let i = 0; i < nodes.length; ++i) {
+            result += nodes[i];
+            if (i < nodes.length - 1) {
+                result += ", ";
+            }
+        }
+
+        result += "]";
+        return result;
+    }
+
+    static nodesStringToVector(nodesString: string): string[] {
+        const nodes: string[] = [];
+
+        const start = nodesString.indexOf("[");
+        const end = nodesString.indexOf("]");
+
+        if (start !== -1 && end !== -1) {
+            const content = nodesString.substring(start + 1, end);
+            const tokens = content.split(",");
+
+            for (const token of tokens) {
+                const node = token.trim();
+                nodes.push(node);
+            }
+        }
+
+        return nodes;
+    }
+
+    static isCorrectFormat(nodesString: string): boolean {
+        if (nodesString.length < 2 || nodesString[0] !== '[' || nodesString[nodesString.length - 1] !== ']') {
+            return false;
+        }
+
+        const content = nodesString.substring(1, nodesString.length - 1).trim();
+        if (content.length === 0) {
+            return false;
+        }
+
+        const tokens = content.split(",");
+        for (const token of tokens) {
+            const trimmedToken = token.trim();
+            const parts = trimmedToken.split(":");
+            if (parts.length !== 2 || parts[0].trim().length === 0 || parts[1].trim().length === 0 || isNaN(Number(parts[1]))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
