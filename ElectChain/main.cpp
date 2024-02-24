4e8f5c81cf31b0fef86fe8c00f584c759d751ad5
alex1591 <alexmatatov4@gmail.com>
2024-02-24 16:40:59 +0200

    update

--- ElectChain/main.cpp
+++ ElectChain/main.cpp
@@ -3,81 +3,70 @@
 #include "Blockchain.h"
 # include "StringUtils.h"
 # include <iostream>
+#include <string>
 
-# define LOCAL_IP "localhost"
-# define PORT "8881"
 
-bool Login(std::shared_ptr<Communicator> cm, std::string peer_address);
+int main() {
+    std::string option;
+    std::shared_ptr<Peer> peer;
 
-static std::string g_userUid = "";
+    while (true) {
+        std::cout << "Options:\n";
+        std::cout << "1. Create Peer\n";
+        std::cout << "2. Connect to Other Peer\n";
+        std::cout << "3. Send Message to All Peers\n";
+        std::cout << "4. Exit\n";
+        std::cout << "Choose an option: ";
+        std::getline(std::cin, option);
 
-int main()
-{
-    bool login = false;
-    std::shared_ptr<net::io_context> ioc_web = std::make_shared<net::io_context>();
-    std::shared_ptr<Communicator> cm;
-
-    try
-    {
-        cm = std::make_shared<Communicator>("localhost", PORT, ioc_web);
+        if (option == "1") {
+            std::string ip;
+            int port;
+            std::cout << "Enter your IP: ";
+            std::getline(std::cin, ip);
+            std::cout << "Enter your port: ";
+            std::cin >> port;
+            std::cin.ignore(); // Ignore the newline character
+            boost::asio::io_context io_context;
+            peer = std::make_shared<Peer>(io_context, tcp::endpoint(boost::asio::ip::address::from_string(ip), port));
+            peer->startAccept();
+            io_context.run();
+        }
+        else if (option == "2") {
+            if (!peer) {
+                std::cout << "You need to create a peer first.\n";
+                continue;
+            }
+            std::string ip;
+            int port;
+            std::cout << "Enter the IP you want to connect to: ";
+            std::getline(std::cin, ip);
+            std::cout << "Enter the port you want to connect to: ";
+            std::cin >> port;
+            std::cin.ignore(); // Ignore the newline character
+            peer->connect(tcp::endpoint(boost::asio::ip::address::from_string(ip), port));
+        }
+        else if (option == "3") {
+            if (!peer) {
+                std::cout << "You need to create a peer first.\n";
+                continue;
+            }
+            std::string message;
+            std::cout << "Enter the message you want to send: ";
+            std::getline(std::cin, message);
+            peer->sendBroadcastMsg(message);
+        }
+        else if (option == "4") {
+            if (peer) {
+                peer->closePeer();
+            }
+            std::cout << "Exiting...\n";
+            break;
+        }
+        else {
+            std::cout << "Invalid option. Please choose again.\n";
+        }
     }
-    catch (const std::exception& e)
-    {
-        std::cerr << e.what();
-        return 1;
-    }
 
-    /*
-        TODO: Create peer - load ip and port from json file if exist
-    */
-    
-    /*
-        TODO: create peer
-        Save in json file
-        peer ip,
-        peer port,
-
-        save in {uid}.pem
-        private key
-        public key
-    */
-    
-    while (!login)
-    {
-        login = Login(cm, "123:123");
-    }
-    
-    ioc_web->run();
     return 0;
-}
-
-bool Login(std::shared_ptr<Communicator> cm, std::string peer_address)
-{
-    std::string uName = "", password = "";
-    std::string res = "";
-
-    std::cout << "Enter username: ";
-    std::cin >> uName;
-    std::cout << "Enter password: ";
-    std::cin >> password;
-
-    res = cm->loginRequest(uName, password, peer_address);
-
-    if (res.rfind(LOGIN_FAILED_CODE, 0) == 0)
-    {
-        return false;
-    }
-    else if (res.rfind(LOGIN_SUCCEEDED_CODE, 0) == 0)
-    {
-        /*nlohmann::json jsonData = StringUtils::strToJson(res.substr(3, res.length()));
-        if (jsonData.find("uid") != jsonData.end())
-        {
-            g_userUid = jsonData['uid'];
-            return true;
-        }*/
-
-        return true;
-    }
-
-    return true;
 }
